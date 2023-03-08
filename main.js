452adbdad8df39ffc1c1c790d21927287a614593
angierobayo9809 <angierobayo09@gmail.com>
2023-03-07 23:11:47 -0500

    Ejercicio 1

---
+++ main.js
@@ -0,0 +1,20 @@
+ let huevoFrito = confirm("¿Quiere el huevo frito?\r\n Aceptar para SI, Cancelar para NO.");
+ 
+ if (huevoFrito)
+ {
+    console.log('Huevo frito.')
+ }
+ else{
+    console.log('Huevo hervido.')
+ }
+
+ let sal = confirm("¿Quiere sal en el huevo?\r\n Aceptar para SI, Cancelar para NO.");
+
+ if (sal)
+ {
+    console.log('hechandole sal al huevo.');
+    console.log('Plato servido.')
+ }
+ else{
+    console.log('Plato servido.')
+ }
